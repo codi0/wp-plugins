8e2ce7764224cdbb0ca39b594fbbe08de6d26b1b
codi0 <hello@codi.io>
2025-08-29 11:22:02 +0100

    Add user vars

--- codi-vars/codi-vars.php
+++ codi-vars/codi-vars.php
@@ -1,28 +1,24 @@
 <?php
 
 /*
-Plugin Name: Codi Vars
-Description: Create variables that can be used as shortcodes in post content and blocks.
-Version: 1.0.0
+Plugin Name: Codi Template Vars
+Description: Unified template variables system for shortcodes and curly-brace tokens.
+Version: 1.1.0
 Author: codi0
-Author URI: https://github.com/codi-si/wp
 */
 
-defined('ABSPATH') or die;
+defined('ABSPATH') || exit;
 
 
-//define constants
-define('CODI_VARS_PLUGIN_FILE', __FILE__);
-define('CODI_VARS_PLUGIN_NAME', basename(__DIR__));
-
-//config vars
+// Define constants
 define('CODI_VARS_ALLOW_PHP', true);
-define('CODI_VARS_BLOCKS_WHITELIST', [ 'core/paragraph' ]);
 define('CODI_VARS_ADMIN_CAP', 'edit_others_posts');
+define('CODI_VARS_BLOCKS_WHITELIST', [ 'core/paragraph' ]);
 
-//set default vars?
+
+/* CONFIG */
+
 if(!get_option('codi_vars')) {
-	//update option
 	update_option('codi_vars', [
 		'year' => '<?= date("Y") ?>',
 		'site-url' => '<?= home_url("{{url}}") ?>',
@@ -33,144 +29,187 @@ if(!get_option('codi_vars')) {
 	]);
 }
 
-//var shortcode
-add_shortcode('var', function($atts) {
+
+/* FUNCTIONS */
+
+function codi_vars_replace_tokens($content, array $params=[]) {
 	//set vars
-	$res = '';
-	$name = null;
-	$atts = $atts ?: [];
-	$vars = get_option('codi_vars');
-	//has name?
-	if(isset($atts['name'])) {
-		$name = $atts['name'];
-		unset($atts['name']);
-	}
-	//valid var?
-	if($name && isset($vars[$name])) {
-		//get result
-		$res = $vars[$name];
+	$vars = get_option('codi_vars', []);
+	//user placeholders
+	$content = preg_replace_callback('/\{user\.([a-zA-Z0-9_]+)\}/', function($m) {
+		//is logged in?
+		if(!is_user_logged_in()) {
+			return '';
+		}
+		//set vars
+		$res = '';
+		$key = $m[1];
+		$user = wp_get_current_user();
+		//is user property?
+		if(isset($user->$key)) {
+			$res = $user->$key;
+		} else if($meta = get_user_meta($user->ID, $key, true)) {
+			$res = $meta;
+		}
+		//return
+		return is_scalar($res) ? esc_html($res) : '';
+	}, $content);
+	//general placeholders
+	$content = preg_replace_callback('/\{([a-zA-Z0-9_\-]+)\}/', function($m) use($params, $vars) {
+		//set vars
+		$key = $m[1];
+		//has var?
+		if(!isset($vars[$key])) {
+			return $m[0];
+		}
+		//set result
+		$res = $vars[$key];
 		//replace params
-		foreach($atts as $k => $v) {
-			$res = str_replace('{{' . $k . '}}', $v ?: '', $res);
+		foreach($params as $k => $v) {
+			$res = str_replace('{{' . $k . '}}', $v !== null ? (string) $v : '', $res);
 		}
 		//remove empty params
-		$res = preg_replace('/{{(.*)}}/U', '', $res);
-		//allow nested shortcodes
-		$res = do_shortcode($res);
-		//might have php code?
-		if(CODI_VARS_ALLOW_PHP && preg_match('/\?\>/', $res)) {
-			//clean opening tag
-			$res = str_replace('&lt;?', '<?', $res);
-			//exec php
-			ob_start();
-			eval("?>$res");
-			$res = ob_get_clean() ?: $res;
-		}
-	}
-	//return
-	return $res;
-});
-
-//block shortcode
-add_shortcode('block', function($atts) {
-	//cast to array
-	$atts = $atts ?: [];
-	//default params
-	$defaults = array_merge([
-		'blockName' => '',
-		'attrs' => [],
-		'innerHTML' => '',
-		'innerContent' => [],
-		'innerBlocks' => [],
-	], $atts);
-	//merge defaults
-	foreach($defaults as $k => $v) {
-		if(!isset($atts[$k])) {
-			$atts[$k] = $v;
-		} else if(is_array($v)) {
-			$atts[$k] = $v ? json_decode($v, true) : [];
-		}
-	}
-	//set name?
-	if(isset($atts['name'])) {
-		$atts['blockName'] = $atts['name'];
-		unset($atts['name']);
-	}
-	//format name?
-	if($atts['blockName']) {
-		//convert camelcase
-		$atts['blockName'] = strtolower(preg_replace('/([A-Z])/', '-$1', $atts['blockName']));
-		//add core prefix?
-		if(strpos($atts['blockName'], '/') === false) {
-			$atts['blockName'] = 'core/' . $atts['blockName'];
-		}
-	}
-	//render?
-	return render_block($atts);
-});
-
-//render shortcodes in whitelisted blocks
-add_filter('render_block', function($content, $block, $context) {
-	//can parse shortcode?
-	if(CODI_VARS_BLOCKS_WHITELIST && !in_array($block['blockName'], CODI_VARS_BLOCKS_WHITELIST)) {
-		return $content;
-	}
-	//parse shortcode
-	return preg_replace_callback('/\[(.*)\]/U', function($match) {
-		return do_shortcode($match[0]);
-	}, $content);
-}, 9999, 3);
-
-//add admin menu
-add_action('admin_menu', function() {
-	//posts submenu
-	add_submenu_page('edit.php', __('Variables'), __('Variables'), CODI_VARS_ADMIN_CAP, 'vars', function() {
-		//save data?
-		if(isset($_POST['vars-name'])) {
-			//get $_POST data
-			$vars = [];
-			$names = stripslashes_deep($_POST['vars-name']);
-			$values = stripslashes_deep($_POST['vars-value']);
-			//loop through names
-			foreach($names as $index => $name) {
-				$vars[$name] = $values[$index];
+		$res = preg_replace('/\{\{[a-zA-Z0-9_\-]+\}\}/', '', $res);
+		//can exec php?
+		if(CODI_VARS_ALLOW_PHP) {
+			//contains php code?
+			if(strpos($res, '<?') !== false && strpos($res, '?>') !== false) {
+				ob_start();
+				eval("?>$res");
+				$res = ob_get_clean() ?: $res;
 			}
-			//save option
-			update_option('codi_vars', $vars);
-		} else {
-			//get data
-			$vars = get_option('codi_vars');
 		}
-		//build html
-		echo '<div class="wrap">';
-		echo '<h1 class="wp-heading-inline">Variables <small>(by <a href="https://github.com/codi-si/wp" target="_blank">codi0</a>)</small></h1>';
-		echo '<p>Define information once and display it in multiple places; including post content, widgets and blocks.</p>';
-		echo '<p>It allows you to update information that changes regularly in one place. You can use text, html and php in values.</p>';
-		echo '<p><i>E.g., using [var name="copyright"] in a post will display <b>' . do_shortcode('[var name="copyright"]') . '</b></i></p>';
-		echo '<br>';
-		echo '<form method="post">';
-		echo '<table id="codi-vars-list" class="wp-list-table widefat fixed striped table-view-list posts">';
-		echo '<tr><th width="200"><b>Name</b></th><th><b>Value</b></th></tr>';
-		foreach($vars as $name => $value) {
-			echo '<tr>';
-			echo '<td><input type="text" name="vars-name[]" value="' . htmlspecialchars($name, ENT_QUOTES) . '" style="width:100%;"></td>';
-			echo '<td><input type="text" name="vars-value[]" value="' . htmlspecialchars($value, ENT_QUOTES) . '" style="width:100%;"></td>';
-			echo '</tr>';
+		//run shortcode?
+		if(strpos($res, '[') !== false && strpos($res, ']') !== false) {
+			$res = do_shortcode($res);
 		}
-		echo '</table>';
-		echo '<p><input type="submit" value="Save Changes" class="button button-primary"> &nbsp; <input type="button" value="Add new" id="codi-vars-add" class="button"></p>';
-		echo '</form>';
-		//build js
-		echo '<script>';
-		?>
-		document.getElementById('codi-vars-add').addEventListener('click', function(e) {
-			var table = document.getElementById('codi-vars-list');
-			var row = document.createElement('tr');
-			row.innerHTML = '<td><input type="text" name="vars-name[]" value="" style="width:100%;"></td><td><input type="text" name="vars-value[]" value="" style="width:100%;"></td>';
-			table.appendChild(row);
-		});
-		<?php
-		echo '</script>';
-		echo '</div>';
+		//re-run replacement?
+		if(strpos($res, '{') !== false && strpos($res, '}') !== false) {
+			$res = codi_vars_replace_tokens($res, $params);
+		}
+		//return
+		return $res;
+	}, $content);
+	//return
+    return $content;
+}
+
+function codi_vars_admin() {
+	//can access?
+	if(!current_user_can(CODI_VARS_ADMIN_CAP)) {
+		wp_die('You do not have permission to access this page.');
+	}
+	//handle save?
+	if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['codi_vars_nonce'])) {
+		//check nonce
+		check_admin_referer('codi_vars_save', 'codi_vars_nonce');
+		//set vars
+		$vars = [];
+		$names = isset($_POST['vars-name'])  ? (array) wp_unslash($_POST['vars-name']) : [];
+		$values = isset($_POST['vars-value']) ? (array) wp_unslash($_POST['vars-value']) : [];
+		//loop through vars
+        foreach($names as $index => $name) {
+			//valid key?
+			if(!$name = sanitize_key($name)) {
+				continue;
+			}
+			//update value
+			$vars[$name] = isset($values[$index]) ? trim($values[$index]) : '';
+		}
+		//save data
+		update_option('codi_vars', $vars);
+		//update UI
+		add_settings_error('codi_vars', 'saved', 'Variables saved', 'updated');
+	}
+	//get current vars
+	$vars = get_option('codi_vars', []);
+	//output admin page
+	echo '<div class="wrap">';
+	echo '<h1 class="wp-heading-inline">Template Variables</h1>';
+	echo '<p>Define information once and display it in multiple places; including post content, widgets and blocks.</p>';
+	echo '<p>It allows you to update information that changes regularly in one place. You can use text or HTML in values.</p>';
+	echo '<p><b>Example:</b> using [var name="copyright"] in a post will display ' . do_shortcode('[var name="copyright"]') . '</p>';
+	echo '<br>';
+	//display errors
+	settings_errors('codi_vars');
+	//display form
+	echo '<form method="post">';
+	wp_nonce_field('codi_vars_save', 'codi_vars_nonce');
+	//display vars table
+	echo '<table id="codi-vars-list" class="wp-list-table widefat fixed striped table-view-list posts">';
+	echo '<tr><th width="200"><b>Name</b></th><th><b>Value</b></th></tr>';
+	//loop through vars
+	foreach($vars as $name => $value) {
+		echo '<tr>';
+		echo '<td><input type="text" name="vars-name[]" value="' . esc_attr($name) . '" style="width:100%;"></td>';
+		echo '<td><input type="text" name="vars-value[]" value="' . esc_attr($value) . '" style="width:100%;"></td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+	echo '<p><input type="submit" value="Save changes" class="button button-primary"> &nbsp; <input type="button" value="Add new" id="codi-vars-add" class="button"></p>';
+	echo '</form>';
+	//add row JS
+	?>
+	<script>
+	document.getElementById('codi-vars-add').addEventListener('click', function() {
+		var table = document.getElementById('codi-vars-list');
+		var row = document.createElement('tr');
+		row.innerHTML = '<td><input type="text" name="vars-name[]" value="" style="width:100%;"></td><td><input type="text" name="vars-value[]" value="" style="width:100%;"></td>';
+		table.appendChild(row);
 	});
+	</script>
+	<?php
+	echo '</div>';
+}
+
+function codi_vars_uninstall() {
+	delete_option('codi_vars');
+}
+
+
+/* HOOKS */
+
+add_shortcode('var', function(array $atts) {
+	//set defaults
+    $atts = array_merge([ 'name' => '', 'default' => '' ], $atts);
+    //extract defaults
+    $name = $atts['name'];
+    $default = $atts['default'];
+    unset($atts['name'], $atts['default']);
+    //run replacement
+    $token = '{' . $name . '}';
+    $res = codi_vars_replace_tokens($token, $atts);
+    //use default?
+    if(!$res || $res === $token) {
+		$res = $default;
+    }
+    //return
+    return $res;
 });
+
+add_filter('render_block', function($content, $block) {
+	//get block name
+    $name = isset($block['blockName']) ? $block['blockName'] : null;
+	//can process block?
+    if(CODI_VARS_BLOCKS_WHITELIST && !in_array($block['blockName'], CODI_VARS_BLOCKS_WHITELIST, true)) {
+        return $content;
+    }
+    //process
+    return codi_vars_replace_tokens($content);
+}, 9999, 2);
+
+add_filter('the_content', function($content) {
+	//skip if blocks?
+    if(function_exists('has_blocks') && has_blocks(get_the_ID())) {
+        return $content;
+    }
+    //process
+	return codi_vars_replace_tokens($content);
+}, 9999);
+
+add_action('admin_menu', function() {
+	add_submenu_page('edit.php', __('Variables'), __('Variables'), CODI_VARS_ADMIN_CAP, 'vars', 'codi_vars_admin');
+	add_submenu_page('edit.php?post_type=page', __('Variables'), __('Variables'), CODI_VARS_ADMIN_CAP, 'vars', 'codi_vars_admin');
+});
+
+register_uninstall_hook(__FILE__, 'codi_vars_uninstall');
